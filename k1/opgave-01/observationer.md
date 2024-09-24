8b6a713533b7b23f34b551e45012b255a55698d8
hgeas <henrik@haskoe.dk>
2024-09-24 17:27:34 +0200

    beregninger på fulde datasæt

--- k1/opgave-01/observationer.md
+++ k1/opgave-01/observationer.md
@@ -1,9 +1,13 @@
 Suggestions:
 . Dont use Excel
-. Gender, height and weight in separate sheet to facilitate auto calculation of bmr
+. consistent column names
+. Always column names in first row
+. Stud_nr, Sex, height and weight in separate sheet to facilitate auto calculation of bmr, remove Sex from ffq, 24-hour and 4-days
 . Individual student data in separate csv files named <student no.>.csv and concatenation into one csv file with proper validations
 . No mean calculation on 4-day csv
-. RI values in separate sheet, actual unit in separate row, units removed from values, min and max values in separate rows
+. RI values in separate sheet, actual unit in separate row, units removed from values, min, recommended and max values (and possibly more i.e. mean, +/-1 SD) in separate rows
+. B12 and Folate names messed up in 4-days and 24-hour
+. Female in upper case !!!
 
 
 
---
+++ k1/opgave-01/opgave-01-01.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import os
+from os import path
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def load_dict(dict_file,sep=';'):
+    with open(dict_file,'r') as f:
+        return dict([(values[0],[float(v) for v in values[1:]]) for values in [l.split(';') for l in f.readlines()]])
+
+def mean_stderr_ref_plot(mean_stderr_df,ref,column_names, mean_std_stderr_columnnames, y_max, chart_title):
+    mean,std,stderr = mean_std_stderr_columnnames
+    ax = mean_stderr_df.plot(ylim=(0, y_max),kind='bar',y=mean, yerr=stderr, rot=45, fill=False, title=chart_title)
+    width=0.5
+    x=0
+    for i,c in enumerate(column_names):
+        rv = np.mean(ref[c])
+        ax.hlines(rv, x - width/2, x + width/2, color='red')
+        x += 1
+
+def read_csv(fname,sep='\t'):
+    return pd.read_csv(path.join(data_dir,fname),sep=sep)
+
+TOTAL_ENERGY = 'Total energy'
+def energy_calculation(input_df,columns):
+    df = input_df.copy()
+    energy_columns = [c.split()[0] for c in columns]
+    energy_percentage_columns = [c+'p' for c in energy_columns]
+
+    # calculate energy, total energy and energy percentage for each column in trial dataset and reference dataset and store in 'energy_columns'
+    # energy
+    for mc,ec in zip(columns,energy_columns):
+        df[ec] = df[mc]*kcal_pr_g[mc]
+    
+    # total energy
+    df[TOTAL_ENERGY] = df[energy_columns].sum(axis=1)
+
+    # and energy percentage
+    for ec,epc in zip(energy_columns,energy_percentage_columns):
+        df[epc] = 100 * df[ec] / df[TOTAL_ENERGY]
+
+    return df, [columns, energy_columns, energy_percentage_columns]
+
+## main script starts here
+try:
+    data_dir = path.dirname(path.abspath(__file__))
+except:
+    # we are in jupyter env.
+    from IPython.display import display, HTML
+    display(HTML("<style>.container { width:100% !important; }</style>"))    
+    data_dir = 'work'
+
+STUDENT_NO_COLNAME='Stud_Nr'
+GENDER_COLNAME='Sex'
+STDDEV_COLNAME='std'
+STDERR_COLNAME='stderr'
+MEAN_COLNAME='mean'
+
+df_ri = read_csv('ri.csv')
+kcal_pr_g = load_dict(path.join(data_dir,"kcal_pr_g.csv"))
+
+#macronutrient_columns = ['Alcohol (g)','Protein (g)','Carbs (g)','Fat (g)']
+micronutrient_columns = ['B1 (Thiamine) (mg)','B2 (Riboflavin) (mg)','B3 (Niacin) (mg)','B5 (Pantothenic Acid) (mg)','B6 (Pyridoxine) (mg)','B12 (Cobalamin) (µg)']
+
+
+student_subset = [str(i) for i in range(200) if i != 11]
+exclude = {
+    'ffq': [],
+    '4-days': [5,11,39,25],
+    '24-hour': []
+}
+for study in ('ffq','24-hour','4-days'):
+    loaded_df = read_csv(f'{study}.csv')
+    loaded_df[STUDENT_NO_COLNAME] = loaded_df[STUDENT_NO_COLNAME].astype(str)
+    loaded_df[GENDER_COLNAME] = loaded_df[GENDER_COLNAME].map(lambda v: v.strip().lower())
+    resulting_subset = [n for n in student_subset if not int(n) in exclude[study]]
+    loaded_df = loaded_df[loaded_df[STUDENT_NO_COLNAME].isin(resulting_subset)].reset_index(drop=True) # reindex is important !!
+    for columns,calculate_energy,mean_legend in ((micronutrient_columns,False,'Mean RI fraction'),): #(macronutrient_columns,True)):
+        df = pd.concat([loaded_df[GENDER_COLNAME], loaded_df[columns].replace(',', '.',regex=True).astype(float)],axis=1) # necessary as decimal separator in CSV files is a mixture of commas and dots
+        
+        # for each input column add a new fraction column
+        fraction_columns = [c+'f' for c in columns]
+        for column, fraction_column in zip(columns,fraction_columns):
+            # lookup ri for each student
+            df_student_ri = df.merge(df_ri, on=[GENDER_COLNAME], how = 'right')[column + '_y']
+            df[fraction_column] = df[column] / df_student_ri
+
+        # calculate mean, STDDEV_COLNAME and stderr
+        mean_std_df = df[fraction_columns].describe().loc[[MEAN_COLNAME,STDDEV_COLNAME]].T
+        mean_std_df = mean_std_df.rename(columns={MEAN_COLNAME: mean_legend})
+        mean_std_df[STDERR_COLNAME] = mean_std_df[STDDEV_COLNAME] / math.sqrt(len(mean_std_df))
+
+        print(mean_std_df)
+        ax = mean_std_df.plot(ylim = (0,3), kind ='bar', y=mean_legend, yerr=STDERR_COLNAME, rot=45, fill=False, title=f'{study} - B1-B12 mean RI fractions')
+        plt.show()
