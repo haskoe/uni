ef0a8e2d8341c560b4d467df17a14999856fde21
haskoe <henrik@haskoe.dk>
2024-09-27 22:27:43 +0200

    total energy RI calculated from BMR

--- k1/opgave-01/r.r
+++ k1/opgave-01/r.r
@@ -6,11 +6,13 @@ STUD_NR <- "Stud_Nr"
 STUDY <- "Study"
 SEX <- "Sex"
 TOTAL_ENERGY <- "Total energy"
-RI_FRAC <- "RI fraction"
+TOTAL_ENERGY_RI <- "Total energy RI"
+RI_FRAC <- "RI"
 NUTRIENT <- "Nutrient"
 NUTRIENT_TYPE <- "Nutrienttype"
 MACRO <- "Macro"
 MICRO <- "Micro"
+BMR <- "BMR.kcal"
 
 ri <- read.csv("ri-denorm.csv", sep = "\t", dec=".", strip.white=TRUE)
 df_empty <- read.csv("new-empty.csv", sep = "\t", dec=".", strip.white=TRUE)
@@ -22,10 +24,13 @@ studies <- list("ffq", "24-hour", "4-days")
 micronutrient_cols <- c("B1.Thiamine.mg","B2.Riboflavin.mg","B3.Niacin.mg","B5.Pantothenic.Acid.mg","B6.Pyridoxine.mg","B12.Cobalamin.µg","Folate.µg","Vitamin.A.µg","Vitamin.C.mg","Vitamin.D.IU","Vitamin.E.mg","Vitamin.K.µg","Calcium.mg","Copper.mg","Iron.mg","Magnesium.mg","Manganese.mg","Phosphorus.mg","Potassium.mg","Selenium.µg","Sodium.mg","Zinc.mg")
 macronutrient_cols <- c("Alcohol.g","Protein.g","Carbs.g","Fat.g")
 
-col_subset <- c(STUD_NR, SEX)
+our_group <- c(8,19,34,35,66)
+
+col_subset <- c(STUD_NR, SEX, BMR)
 res <- df_empty
 for (study in studies) {
-  df_csv  <- read.csv(paste(study,".csv",sep=""), sep = "\t", dec=".", strip.white=TRUE)
+  print(study)
+  df_csv  <- read.csv(paste(study,".fixed.csv",sep=""), sep = "\t", dec=".", strip.white=TRUE)
   
   # res: Stud_Nr, Sex, Nutrient, RI factor
   
@@ -35,6 +40,7 @@ for (study in studies) {
     output_cols <- c()
     tmp <-df_csv %>% select(col_subset)
     for (colname in cols) {
+      print(colname)
       colname_frac <- paste(colname,"f",sep="")
       output_cols <- append( output_cols, colname_frac)
       colname_space <- gsub("\\.", " ", colname)
@@ -51,11 +57,13 @@ for (study in studies) {
   
         joined <- dplyr::left_join(tmp, ref, by = "Sex", keep = TRUE)
         tmp[colname_frac] <- joined[colname_frac] / joined$Refvalue
+        tmp[TOTAL_ENERGY_RI] <- 0
       }
     }
     if (nutrient_type == MACRO) {
       # calculate total energy
       tmp[TOTAL_ENERGY] <- rowSums(tmp[,output_cols])
+      tmp[TOTAL_ENERGY_RI] <- tmp[TOTAL_ENERGY] / tmp[BMR]
     
       # and fractions of total energy
       for (colname in output_cols) {
@@ -66,7 +74,7 @@ for (study in studies) {
     # append rows to resulting dataframe for each nutrient
     # could have used pivot_longer !!
     for (colname in output_cols) {
-      res <- rbind(res, setNames( cbind(tmp[c(STUD_NR,SEX)],study,colname,nutrient_type, tmp[colname]), names(res)))
+      res <- rbind(res, setNames( cbind(tmp[c(STUD_NR,SEX)],study,colname,nutrient_type, tmp[colname],tmp[TOTAL_ENERGY_RI]), names(res)))
     }
     
     nutrient_type <- MICRO
