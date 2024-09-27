bfe59a3e46375f6b15114d192f1cfd0a235320a5
haskoe <henrik@haskoe.dk>
2024-09-27 08:54:53 +0200

    r: now with fractions only dataframe

--- k1/opgave-01/r.r
+++ k1/opgave-01/r.r
@@ -4,23 +4,24 @@ library(dplyr)
 ri <- read.csv("ri-denorm.csv", sep = "\t", dec=".", strip.white=TRUE)
 
 studies <- list("ffq", "24-hour", "4-days")
-cols <- list("B1.Thiamine.mg") #,"B2.Riboflavin.mg","B3.Niacin.mg","B5.Pantothenic.Acid.mg","B6.Pyridoxine.mg","B12.Cobalamin.µg")
+cols <- list("B1.Thiamine.mg","B2.Riboflavin.mg","B3.Niacin.mg","B5.Pantothenic.Acid.mg","B6.Pyridoxine.mg","B12.Cobalamin.µg")
 
 l_df_studies <- list()
 for (study in studies) {
-  df  <- read.csv(paste(study,".csv",sep=""), sep = "\t", dec=".", strip.white=TRUE)
-
+  df_csv  <- read.csv(paste(study,".csv",sep=""), sep = "\t", dec=".", strip.white=TRUE)
+  res <- df_csv["Sex"]
   for (colname in cols) {
+    colname_frac <- paste(colname,"f",sep="")
     colname_space <- gsub("\\.", " ", colname)
     
     # filter ri to get values only for selected nutrient
     ref <- ri[ri$Refname == colname_space,]
                     
-    col_subset <- c("Sex", v=colname)
-    df <- X4_days %>% select(col_subset)
+    col_subset <- c("Sex", colname)
+    tmp <-df_csv %>% select(col_subset)
     
-    joined <- dplyr::left_join(df, ref, by = "Sex", keep = TRUE)
-    joined$frac <- joined$v * joined$Refvalue
-    View(joined)
+    joined <- dplyr::left_join(tmp, ref, by = "Sex", keep = TRUE)
+    res[colname_frac] <- joined[colname] * joined$Refvalue
   }
 }
+View(joined)
